ea99c1c101e736c7e22d8681c632e68519afd36c
Michael Mora M <mmora@teledolar.net>
2018-09-25 15:28:53 -0600

    Web -> 6 Done
    Web -> 6 Done

--- Web/6-ExpressFileUpload/main.js
+++ Web/6-ExpressFileUpload/main.js
@@ -25,7 +25,20 @@ app.post('/file_upload',  function (req, res) {
   // https://www.npmjs.com/package/express-fileupload
   // Ponga su codigo aqui
 
+//  console.log(req.files);
 
+
+//console.log(req.files['archivo'];
+  if (!req.files['archivo'])
+    return res.status(400).send('No files were uploaded.');
+
+let sampleFile = req.files['archivo'];
+  sampleFile.mv(__dirname+'/test.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 
 });
 
