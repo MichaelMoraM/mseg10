58b40947af5b4226835a6575ce71ba214f2b4722
MichaelMoraM <40668449+MichaelMoraM@users.noreply.github.com>
2018-09-29 10:04:34 -0600

    LAb 1
    LAb 1

---
+++ Entregables/7.2-Streams/main.js
@@ -0,0 +1,38 @@
+/*  Universidad Cenfotec
+    Maestria en Ciberseguridad
+    Creado por: izamora@ucenfotec.ac.cr
+
+    Ejemplo 8.2: Pipe Streams
+*/
+
+/// PIPING
+/*var fs = require("fs");
+
+// Create a readable stream
+var readerStream = fs.createReadStream('a-wing.txt');
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('j-type-327.txt');
+
+// Se lee input.txt y se agrega la data a output.txt
+readerStream.pipe(writerStream);*/
+
+// Salvar el pipe TODO.
+// Ejericio: trate de salvar el texto en un nuevo archivo
+
+
+
+var concat = require('concat-files');
+
+concat([
+  'a-wing.txt',
+  'j-type-327.txt'
+], 'test.txt', function(err) {
+  if (err) throw err
+  console.log('done');
+});
+
+
+// fin de codigo
+
+console.log("Program Ended");
